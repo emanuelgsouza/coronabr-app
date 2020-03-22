d43c823b160db040e7130229c7e7b2de57ab503b
Emanuel Souza <emanuelgdesouza@gmail.com>
2020-03-22 10:51:34 -0300

    update page description

--- pages/index.vue
+++ pages/index.vue
@@ -11,20 +11,14 @@
           Este projeto tem como objetivo trazer um acompanhamento dos casos do COVID-19 no Brasil. Sendo assim, primeiramente você poderá ver uma linha do tempo mostrando a evolução dos casos oficiais no Brasil.
         </p>
 
-        <p>
-          <strike>Os dados deste projeto vem diretamente do site oficial do</strike> <a href="https://saude.gov.br/" title="Visite o site oficial do Ministério da Saúde">Ministério da Saúde (MS)</a> destinado a dar uma visualização e o estado atual da epidemia no Brasil. Você pode conferir o site oficial, que mostra algumas outras informações, tais como uma tabela mostrando os dados por país, no endereço <a href="http://plataforma.saude.gov.br/novocoronavirus/" title="Visite o site oficial para acompanhamento do COVID-19 feito pelo Ministério da Saúde">http://plataforma.saude.gov.br/novocoronavirus/</a>
-        </p>
+        <p>A fonte de dados para este projeto vem da <a href="https://ourworldindata.org/coronavirus-source-data" title="Confira o site oficial da Our World in Data">Our World in Data</a>, a qual usa os da dados da OMS (Organização Mundial da Saúde). Esta mesma fonte também é usada pelo equipe do Estadão em seu projeto de acompanhamento diário dos casos de CODIV-19 no Brasil (vide link abaixo). <span class="has-text-weight-bold">A atualização dos dados é diária</span></p>
 
         <p>
-          A partir do dia 18 de Março o site oficial do MS apresentou instabilidades e desde então os dados não foram mais atualizados. Sendo assim, <span class="has-text-weight-bold">a fonte de dados para este projeto mudou</span>. Encontramos um outro projeto, usado pela Equipe do Estadão (vide link abaixo) para analisarmos e tirarmos as informações de que precisamos para acompanhar a evolução no COVID-19 no Brasil. Reiterando, portanto, a fonte de dados atual é a do site da <a href="https://ourworldindata.org/coronavirus-source-data" title="Confira o site oficial da Our World in Data">Our World in Data</a> e <span class="has-text-weight-bold">a atualização dos dados é diária</span>.
-        </p>
-
-        <p>
-          Você conferir também alguns outros projetos para acompanhamento da situação no Brasil e no mundo:
+          Você conferir também outros projetos para acompanhamento da situação no Brasil e no mundo:
         </p>
         <ul>
           <li> <a href="https://github.com/belisards/coronabr" title="Configura a base de dados no Github">Base de dados em CSV</a> atualizado diariamente feita pelo <a href="https://twitter.com/belisards" title="Visite o Twitter do Adriano Belisário">Adriano Belisário</a> </li>
-          <li><a href="http://plataforma.saude.gov.br/novocoronavirus/" title="Visite o site oficial para acompanhamento do COVID-19 feito pelo Ministério da Saúde">Site oficial</a> para acompanhamento dos casos pelo MS</li>
+          <li><a href="http://plataforma.saude.gov.br/novocoronavirus/" title="Visite o site oficial para acompanhamento do COVID-19 feito pelo Ministério da Saúde">Site oficial</a> para acompanhamento dos casos pelo MS (<span class="has-text-weight-bold">Até o momento sem atualização</span> )</li>
           <li><a href="https://coronavirus.jhu.edu/map.html" title="Visite o mapa desenvolvido pela Universidade Johns Hopkins">Mapa do Coronavirus</a> no mundo desenvolvido pela Universidade Johns Hopkins</li>
           <li><a href="https://arte.estadao.com.br/ciencia/novo-coronavirus/monitor-pandemia/" title="Confira o Monitor do COVID-19 desenvolvido pela equipe do Estadão"> Monitor do COVID-19 </a> desenvolvido pela equipe do Estadão</li>
           <li> <a href="https://www.who.int/emergencies/diseases/novel-coronavirus-2019/situation-reports/" title="Confira o situation report">Situation Report</a> da Organização Mundial da Saúde</li>
