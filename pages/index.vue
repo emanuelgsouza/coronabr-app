048531d67b47f555e582f50f9cf54026484d5682
Emanuel Souza <emanuelgdesouza@gmail.com>
2020-03-22 11:12:19 -0300

    more updates in description

--- pages/index.vue
+++ pages/index.vue
@@ -14,10 +14,10 @@
         <p>A fonte de dados para este projeto vem da <a href="https://ourworldindata.org/coronavirus-source-data" title="Confira o site oficial da Our World in Data">Our World in Data</a>, a qual usa os da dados da OMS (Organização Mundial da Saúde). Esta mesma fonte também é usada pelo equipe do Estadão em seu projeto de acompanhamento diário dos casos de CODIV-19 no Brasil (vide link abaixo). <span class="has-text-weight-bold">A atualização dos dados é diária</span></p>
 
         <p>
-          Você conferir também outros projetos para acompanhamento da situação no Brasil e no mundo:
+          Você pode conferir também outros projetos para acompanhamento da situação no Brasil e no mundo:
         </p>
         <ul>
-          <li> <a href="https://github.com/belisards/coronabr" title="Configura a base de dados no Github">Base de dados em CSV</a> atualizado diariamente feita pelo <a href="https://twitter.com/belisards" title="Visite o Twitter do Adriano Belisário">Adriano Belisário</a> </li>
+          <li> <a href="https://github.com/belisards/coronabr" title="Configura a base de dados no Github">Base de dados em CSV</a> atualizado diariamente pelo <a href="https://twitter.com/belisards" title="Visite o Twitter do Adriano Belisário">Adriano Belisário</a> </li>
           <li><a href="http://plataforma.saude.gov.br/novocoronavirus/" title="Visite o site oficial para acompanhamento do COVID-19 feito pelo Ministério da Saúde">Site oficial</a> para acompanhamento dos casos pelo MS (<span class="has-text-weight-bold">Até o momento sem atualização</span> )</li>
           <li><a href="https://coronavirus.jhu.edu/map.html" title="Visite o mapa desenvolvido pela Universidade Johns Hopkins">Mapa do Coronavirus</a> no mundo desenvolvido pela Universidade Johns Hopkins</li>
           <li><a href="https://arte.estadao.com.br/ciencia/novo-coronavirus/monitor-pandemia/" title="Confira o Monitor do COVID-19 desenvolvido pela equipe do Estadão"> Monitor do COVID-19 </a> desenvolvido pela equipe do Estadão</li>
